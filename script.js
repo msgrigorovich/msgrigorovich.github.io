0c67f5188dadc03969ea58901cc48c165d1e6a3d
d.grigorovich <d.grigorovich@strikerz.inc>
2025-04-07 00:04:14 +0400

    ### Fixed: Delay animation

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 
 function startTyping() {
     const el = document.querySelector('.typing-text');
+  
+    // Start typing
     el.style.animation = 'typing 1.5s steps(13, end) forwards, blink 0.7s step-end infinite';
   
+    // Stay text on a screen for 1.5 sec after typing
     setTimeout(() => {
       el.style.animation = 'none';
+      el.textContent = ''; // clear text
       void el.offsetWidth;
-      setTimeout(startTyping, 1500);
-    }, 1500);
+  
+      // After 0.1 sec return line and restart animation
+      setTimeout(() => {
+        el.textContent = 'Hello, World!';
+        startTyping();
+      }, 100);
+    }, 3000); // 1.5 (typing) + 1.5 (pause)
   }
   
-  startTyping();
-  
+  startTyping();  
