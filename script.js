dcdac08965beb8d0bda169cc3e4211805d6d057b
d.grigorovich <d.grigorovich@strikerz.inc>
2025-04-08 00:18:58 +0400

    ### Fixed: some header's bugs

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
 function startTyping() {
-    const el = document.querySelector('.typing-text');
-    el.style.animation = 'typing 1.5s steps(13, end) forwards, blink 0.7s step-end infinite';
-  
-    setTimeout(() => {
-      el.style.animation = 'none';
-      void el.offsetWidth;
-      setTimeout(startTyping, 1500);
-    }, 1500);
-  }
-  
-  startTyping();
-  
+  const el = document.querySelector('.typing-text');
+  el.style.animation = 'typing 1.5s steps(13, end) forwards, blink 0.7s step-end infinite';
+
+  setTimeout(() => {
+    el.style.animation = 'none';
+    void el.offsetWidth;
+    setTimeout(startTyping, 1500);
+  }, 1500);
+}
+
+startTyping();
