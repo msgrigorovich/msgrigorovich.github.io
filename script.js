e101d72621a02fc99eb1790b15c7fcbb83f7593e
d.grigorovich <d.grigorovich@strikerz.inc>
2025-04-07 19:53:09 +0400

    ### Fixed: Revert and Fix merge conflicts

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 
 function startTyping() {
     const el = document.querySelector('.typing-text');
-  
-    // Start typing
     el.style.animation = 'typing 1.5s steps(13, end) forwards, blink 0.7s step-end infinite';
   
-    // Stay text on a screen for 1.5 sec after typing
     setTimeout(() => {
       el.style.animation = 'none';
-      el.textContent = ''; // clear text
       void el.offsetWidth;
-  
-      // After 0.1 sec return line and restart animation
-      setTimeout(() => {
-        el.textContent = 'Hello, World!';
-        startTyping();
-      }, 100);
-    }, 3000); // 1.5 (typing) + 1.5 (pause)
+      setTimeout(startTyping, 1500);
+    }, 1500);
   }
   
-  startTyping();  
+  startTyping();
+  
