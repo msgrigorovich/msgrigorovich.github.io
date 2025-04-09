7ab20b1b5912248b40933af17089490ee08108ed
d.grigorovich <d.grigorovich@strikerz.inc>
2025-04-09 23:03:12 +0400

    ### Added: ResumePage and Transition on them

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 
-function startTyping() {
-  const el = document.querySelector('.typing-text');
-  el.style.animation = 'typing 1.5s steps(13, end) forwards, blink 0.7s step-end infinite';
+const el = document.querySelector('.typing-text');
+const text = "Hello, World!";
+let index = 0;
+let typingForward = true;
 
-  setTimeout(() => {
-    el.style.animation = 'none';
-    void el.offsetWidth;
-    setTimeout(startTyping, 1500);
-  }, 1500);
+function typeEffect() {
+  if (!el) return;
+  if (typingForward) {
+    if (index <= text.length) {
+      el.textContent = text.substring(0, index);
+      index++;
+      setTimeout(typeEffect, 100);
+    } else {
+      typingForward = false;
+      setTimeout(typeEffect, 2000);
+    }
+  } else {
+    if (index >= 0) {
+      el.textContent = text.substring(0, index);
+      index--;
+      setTimeout(typeEffect, 50);
+    } else {
+      typingForward = true;
+      setTimeout(typeEffect, 500);
+    }
+  }
 }
 
-startTyping();
+typeEffect();
